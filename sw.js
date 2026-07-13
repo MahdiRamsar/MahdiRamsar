035ece0bb0aba1b47a38b6d818718aec998cac11
MahdiRamsar <rahimian.mohammadmahdi@gmail.com>
2026-07-13 08:59:46 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,20 +3,33 @@ const urlsToCache = [
     '/',
     '/index.html',
     '/1.jpg',
-    'https://cdn.jsdelivr.net/gh/rastikerdar/vazirmatn@v33.003/Vazirmatn-font-face.css',
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
+    '/1.webp',
+    '/1.avif'
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener('install', event => {
     event.waitUntil(
-        caches.open(CACHE_NAME)
-        .then((cache) => cache.addAll(urlsToCache))
+        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
     );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
-        .then((response) => response || fetch(event.request))
+            .then(response => response || fetch(event.request))
     );
 });
